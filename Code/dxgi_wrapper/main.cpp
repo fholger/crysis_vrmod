71d7d063dc37f39fc21f6770e588a8ed27dc629d
Holger Frydrych <holger@frydrych.org>
2023-03-28 21:26:42 +0200

    Add dxgi wrapper that redirects to CreateDXGIFactory1 to enable resource sharing for OpenVR

---
+++ Code/dxgi_wrapper/main.cpp
@@ -0,0 +1,87 @@
+#include <dxgi.h>
+#include <string>
+
+namespace
+{
+	HMODULE dxgiHandle = nullptr;
+
+	typedef HRESULT(*LP_CreateDXGIFactory1)(REFIID, void**);
+	typedef HRESULT(*LP_CreateDXGIFactory2)(UINT, REFIID, void**);
+	typedef HRESULT(*LP_DXGIDeclareAdapterRemovalSupport)();
+	typedef HRESULT(*LP_DXGIGetDebugInterface1)(UINT, REFIID, void**);
+
+	LP_CreateDXGIFactory1 g_pfnCreateDXGIFactory1 = nullptr;
+	LP_CreateDXGIFactory2 g_pfnCreateDXGIFactory2 = nullptr;
+	LP_DXGIDeclareAdapterRemovalSupport g_pfnDXGIDeclareAdapterRemovalSupport = nullptr;
+	LP_DXGIGetDebugInterface1 g_pfnDXGIGetDebugInterface1 = nullptr;
+
+	bool LoadSystemDXGI()
+	{
+		if (dxgiHandle)
+			return true;
+
+		wchar_t buf[MAX_PATH + 1];
+		GetSystemDirectory(buf, sizeof(buf));
+		std::wstring dllPath = buf;
+		dllPath += L"\\dxgi.dll";
+
+		dxgiHandle = LoadLibrary(dllPath.c_str());
+		if (!dxgiHandle)
+			return false;
+
+		g_pfnCreateDXGIFactory1 = (LP_CreateDXGIFactory1)GetProcAddress(dxgiHandle, "CreateDXGIFactory1");
+		g_pfnCreateDXGIFactory2 = (LP_CreateDXGIFactory2)GetProcAddress(dxgiHandle, "CreateDXGIFactory2");
+		g_pfnDXGIDeclareAdapterRemovalSupport = (LP_DXGIDeclareAdapterRemovalSupport)GetProcAddress(dxgiHandle, "DXGIDeclareAdapterRemovalSupport");
+		g_pfnDXGIGetDebugInterface1 = (LP_DXGIGetDebugInterface1)GetProcAddress(dxgiHandle, "DXGIGetDebugInterface1");
+
+		return true;
+	}
+}
+
+extern "C" {
+	HRESULT __stdcall CreateDXGIFactory2(UINT Flags, REFIID riid, void** ppFactory)
+	{
+		if (!LoadSystemDXGI())
+		{
+			return E_FAIL;
+		}
+
+		return g_pfnCreateDXGIFactory2(Flags, riid, ppFactory);
+	}
+
+	HRESULT __stdcall CreateDXGIFactory1(REFIID riid, void** ppFactory)
+	{
+		if (!LoadSystemDXGI())
+		{
+			return E_FAIL;
+		}
+
+		return g_pfnCreateDXGIFactory1(riid, ppFactory);
+	}
+
+	HRESULT __stdcall CreateDXGIFactory(REFIID riid, void **ppFactory)
+	{
+		// redirect to CreateDXGIFactory1
+		return CreateDXGIFactory1(riid, ppFactory);
+	}
+
+	HRESULT __stdcall DXGIDeclareAdapterRemovalSupport()
+	{
+		if (!LoadSystemDXGI())
+		{
+			return E_FAIL;
+		}
+
+		return g_pfnDXGIDeclareAdapterRemovalSupport();
+	}
+
+	HRESULT __stdcall DXGIGetDebugInterface1(UINT Flags, REFIID riid, void **pDebug)
+	{
+		if (!LoadSystemDXGI())
+		{
+			return E_FAIL;
+		}
+
+		return g_pfnDXGIGetDebugInterface1(Flags, riid, pDebug);
+	}
+}
--- install.bat
+++ install.bat
@@ -8,7 +8,9 @@ set VRMOD_INSTALL_DIR=%CRYSIS_INSTALL_DIR%\Mods\VRMod
 if not exist %VRMOD_INSTALL_DIR% (mkdir %VRMOD_INSTALL_DIR%)
 
 xcopy /i /y %SOURCE_DIR%\*.xml %VRMOD_INSTALL_DIR%
-xcopy /i /y %SOURCE_DIR%\Bin64\*.dll %VRMOD_INSTALL_DIR%\Bin64
+xcopy /y %SOURCE_DIR%\Bin64\VRMod.dll %VRMOD_INSTALL_DIR%\Bin64
+xcopy /y %SOURCE_DIR%\Bin64\dxgi.dll %CRYSIS_INSTALL_DIR%\Bin64
+xcopy /y %SOURCE_DIR%\Bin64\openvr_api.dll %CRYSIS_INSTALL_DIR%\Bin64
 xcopy /y %SOURCE_DIR%\LaunchVRMod.bat %VRMOD_INSTALL_DIR%
 
 goto eof
