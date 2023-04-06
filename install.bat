92220de31ac52d7b74d35bf76d72f20578bb7a38
Holger Frydrych <holger@frydrych.org>
2023-04-06 18:28:13 +0200

    Disable motion blur, depth of field and edge AA in the game settings templates, as they don't look good or interfere with VR rendering

--- install.bat
+++ install.bat
@@ -13,6 +13,7 @@ rem xcopy /y %SOURCE_DIR%\Bin64\dxgi.dll %CRYSIS_INSTALL_DIR%\Bin64
 xcopy /y %SOURCE_DIR%\Bin64\d3d10.dll %CRYSIS_INSTALL_DIR%\Bin64
 xcopy /y %SOURCE_DIR%\Bin64\openvr_api.dll %CRYSIS_INSTALL_DIR%\Bin64
 xcopy /y %SOURCE_DIR%\Bin64\Crysis.exe %CRYSIS_INSTALL_DIR%\Bin64
+xcopy /i /s /y %SOURCE_DIR%\Game %VRMOD_INSTALL_DIR%\Game
 xcopy /y %SOURCE_DIR%\LaunchVRMod.bat %CRYSIS_INSTALL_DIR%
 
 goto eof
