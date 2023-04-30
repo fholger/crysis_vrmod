6c47bd36ed3081ada4b316045b678fccdc18ffbd
Holger Frydrych <holger@frydrych.org>
2023-04-30 15:20:40 +0200

    Enable motion controller thumbstick inputs for basic player movement

--- Code/VR/OpenXRInput.cpp
+++ Code/VR/OpenXRInput.cpp
@@ -1,6 +1,10 @@
 #include "StdAfx.h"
 #include "OpenXRInput.h"
 
+#include "GameActions.h"
+#include "IPlayerInput.h"
+#include "Player.h"
+
 extern bool XR_CheckResult(XrResult result, const char* description, XrInstance instance = nullptr);
 
 class SuggestedProfileBinding
@@ -41,13 +45,18 @@ void OpenXRInput::Init(XrInstance instance, XrSession session)
 
 	CreateInputActions();
 	SuggestBindings();
+
+	XrSessionActionSetsAttachInfo attachInfo{ XR_TYPE_SESSION_ACTION_SETS_ATTACH_INFO };
+	attachInfo.actionSets = &m_ingameSet;
+	attachInfo.countActionSets = 1;
+	XR_CheckResult(xrAttachSessionActionSets(m_session, &attachInfo), "attaching action set", m_instance);
 }
 
 void OpenXRInput::Shutdown()
 {
 	xrDestroyAction(m_primaryFire);
-	xrDestroyAction(m_stick[0]);
-	xrDestroyAction(m_stick[1]);
+	xrDestroyAction(m_moveX);
+	xrDestroyAction(m_moveY);
 	xrDestroyAction(m_controller[0]);
 	xrDestroyAction(m_controller[1]);
 	xrDestroyActionSet(m_ingameSet);
@@ -66,6 +75,8 @@ void OpenXRInput::Update()
 	syncInfo.countActiveActionSets = 1;
 	syncInfo.activeActionSets = &activeSet;
 	XR_CheckResult(xrSyncActions(m_session, &syncInfo), "syncing actions");
+
+	UpdatePlayerMovement();
 }
 
 void OpenXRInput::CreateInputActions()
@@ -81,13 +92,19 @@ void OpenXRInput::CreateInputActions()
 	strcpy(createInfo.localizedActionName, "Right Controller");
 	XR_CheckResult(xrCreateAction(m_ingameSet, &createInfo, &m_controller[1]), "creating controller pose action");
 
-	createInfo.actionType = XR_ACTION_TYPE_VECTOR2F_INPUT;
-	strcpy(createInfo.actionName, "left_joystick");
-	strcpy(createInfo.localizedActionName, "Left Joystick");
-	XR_CheckResult(xrCreateAction(m_ingameSet, &createInfo, &m_stick[0]), "creating thumbstick action");
-	strcpy(createInfo.actionName, "right_joystick");
-	strcpy(createInfo.localizedActionName, "Right Joystick");
-	XR_CheckResult(xrCreateAction(m_ingameSet, &createInfo, &m_stick[1]), "creating thumbstick action");
+	createInfo.actionType = XR_ACTION_TYPE_FLOAT_INPUT;
+	strcpy(createInfo.actionName, "move_x");
+	strcpy(createInfo.localizedActionName, "Move X");
+	XR_CheckResult(xrCreateAction(m_ingameSet, &createInfo, &m_moveX), "creating movement action");
+	strcpy(createInfo.actionName, "move_y");
+	strcpy(createInfo.localizedActionName, "Move Y");
+	XR_CheckResult(xrCreateAction(m_ingameSet, &createInfo, &m_moveY), "creating movement action");
+	strcpy(createInfo.actionName, "rotate_yaw");
+	strcpy(createInfo.localizedActionName, "Rotate Yaw");
+	XR_CheckResult(xrCreateAction(m_ingameSet, &createInfo, &m_rotateYaw), "creating movement action");
+	strcpy(createInfo.actionName, "rotate_pitch");
+	strcpy(createInfo.localizedActionName, "Rotate Pitch");
+	XR_CheckResult(xrCreateAction(m_ingameSet, &createInfo, &m_rotatePitch), "creating movement action");
 }
 
 void OpenXRInput::SuggestBindings()
@@ -96,16 +113,20 @@ void OpenXRInput::SuggestBindings()
 	knuckles.AddBinding(m_primaryFire, "/user/hand/right/input/trigger");
 	knuckles.AddBinding(m_controller[0], "/user/hand/left/input/aim");
 	knuckles.AddBinding(m_controller[1], "/user/hand/right/input/aim");
-	knuckles.AddBinding(m_stick[0], "/user/hand/left/input/thumbstick");
-	knuckles.AddBinding(m_stick[1], "/user/hand/right/input/thumbstick");
+	knuckles.AddBinding(m_moveX, "/user/hand/left/input/thumbstick/x");
+	knuckles.AddBinding(m_moveY, "/user/hand/left/input/thumbstick/y");
+	knuckles.AddBinding(m_rotateYaw, "/user/hand/right/input/thumbstick/x");
+	knuckles.AddBinding(m_rotatePitch, "/user/hand/right/input/thumbstick/y");
 	knuckles.SuggestBindings("/interaction_profiles/valve/index_controller");
 
 	SuggestedProfileBinding touch(m_instance);
 	touch.AddBinding(m_primaryFire, "/user/hand/right/input/trigger");
 	touch.AddBinding(m_controller[0], "/user/hand/left/input/aim");
 	touch.AddBinding(m_controller[1], "/user/hand/right/input/aim");
-	touch.AddBinding(m_stick[0], "/user/hand/left/input/thumbstick");
-	touch.AddBinding(m_stick[1], "/user/hand/right/input/thumbstick");
+	touch.AddBinding(m_moveX, "/user/hand/left/input/thumbstick/x");
+	touch.AddBinding(m_moveY, "/user/hand/left/input/thumbstick/y");
+	touch.AddBinding(m_rotateYaw, "/user/hand/right/input/thumbstick/x");
+	touch.AddBinding(m_rotatePitch, "/user/hand/right/input/thumbstick/y");
 	touch.SuggestBindings("/interaction_profiles/oculus/touch_controller");
 }
 
@@ -116,4 +137,46 @@ void OpenXRInput::CreateBooleanAction(XrActionSet actionSet, XrAction* action, c
 	strcpy(createInfo.actionName, name);
 	strcpy(createInfo.localizedActionName, description);
 	XR_CheckResult(xrCreateAction(actionSet, &createInfo, action), "creating boolean action");
+}
+
+void OpenXRInput::UpdatePlayerMovement()
+{
+	CPlayer *pPlayer = static_cast<CPlayer *>(gEnv->pGame->GetIGameFramework()->GetClientActor());
+	if (!pPlayer)
+		return;
+	IPlayerInput* input = pPlayer->GetPlayerInput();
+	if (!input)
+		return;
+
+	XrActionStateFloat state{ XR_TYPE_ACTION_STATE_FLOAT };
+	XrActionStateGetInfo getInfo{ XR_TYPE_ACTION_STATE_GET_INFO };
+	getInfo.subactionPath = XR_NULL_PATH;
+
+	getInfo.action = m_moveX;
+	xrGetActionStateFloat(m_session, &getInfo, &state);
+	if (state.isActive)
+	{
+		input->OnAction(g_pGameActions->xi_movex, eAAM_Always, state.currentState);
+	}
+
+	getInfo.action = m_moveY;
+	xrGetActionStateFloat(m_session, &getInfo, &state);
+	if (state.isActive)
+	{
+		input->OnAction(g_pGameActions->xi_movey, eAAM_Always, state.currentState);
+	}
+
+	getInfo.action = m_rotateYaw;
+	xrGetActionStateFloat(m_session, &getInfo, &state);
+	if (state.isActive)
+	{
+		input->OnAction(g_pGameActions->xi_rotateyaw, eAAM_Always, state.currentState);
+	}
+
+	getInfo.action = m_rotatePitch;
+	xrGetActionStateFloat(m_session, &getInfo, &state);
+	if (state.isActive)
+	{
+		input->OnAction(g_pGameActions->xi_rotatepitch, eAAM_Always, state.currentState);
+	}
 }
--- Code/VR/OpenXRInput.h
+++ Code/VR/OpenXRInput.h
@@ -15,10 +15,14 @@ private:
 
 	XrActionSet m_ingameSet = nullptr;
 	XrAction m_controller[2] = {};
-	XrAction m_stick[2] = {};
+	XrAction m_moveX = nullptr;
+	XrAction m_moveY = nullptr;
+	XrAction m_rotateYaw = nullptr;
+	XrAction m_rotatePitch = nullptr;
 	XrAction m_primaryFire = nullptr;
 
 	void CreateInputActions();
 	void SuggestBindings();
 	void CreateBooleanAction(XrActionSet actionSet, XrAction* action, const char* name, const char* description);
+	void UpdatePlayerMovement();
 };
