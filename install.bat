54ec293a2a6654a0bece04fd16fc8b0063787c28
Holger Frydrych <holger@frydrych.org>
2025-01-15 00:56:45 +0100

    fixup! Rework HUD tutorial messages for VR (initial draft)

--- install.bat
+++ install.bat
@@ -7,10 +7,6 @@ set SOURCE_DIR=%~dp0
 set VRMOD_INSTALL_DIR=%CRYSIS_INSTALL_DIR%\Mods\VRMod
 if not exist %VRMOD_INSTALL_DIR%\Bin64 (mkdir %VRMOD_INSTALL_DIR%\Bin64)
 
-cd %SOURCE_DIR%\Game\Localized
-7z a english.zip .\Languages
-rename english.zip english.pak
-
 xcopy /i /y %SOURCE_DIR%\*.xml %VRMOD_INSTALL_DIR%
 xcopy /y %SOURCE_DIR%\Bin64\VRMod.dll %VRMOD_INSTALL_DIR%\Bin64
 xcopy /y %SOURCE_DIR%\Bin64\CrysisVR.exe %CRYSIS_INSTALL_DIR%\Bin64
@@ -18,9 +14,6 @@ xcopy /i /s /y %SOURCE_DIR%\Game %VRMOD_INSTALL_DIR%\Game
 xcopy /y %SOURCE_DIR%\system.vrsample.cfg %CRYSIS_INSTALL_DIR%
 xcopy /y %SOURCE_DIR%\README.md %VRMOD_INSTALL_DIR%
 xcopy /y %SOURCE_DIR%\LICENSE.txt %VRMOD_INSTALL_DIR%
-
-rmdir /s /q %VRMOD_INSTALL_DIR%\Game\Localized\Languages
-del /q %SOURCE_DIR%\Game\Localized\english.pak
 
 goto eof
 
