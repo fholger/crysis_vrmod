1d98436a8db2d0bb214a5d382134e22ae505c3e2
Holger Frydrych <holger@frydrych.org>
2025-01-28 23:17:04 +0100

    Include ForceTube DLLs in install

--- install.bat
+++ install.bat
@@ -11,6 +11,7 @@ xcopy /i /y %SOURCE_DIR%\*.xml %VRMOD_INSTALL_DIR%
 xcopy /y %SOURCE_DIR%\Bin64\VRMod.dll %VRMOD_INSTALL_DIR%\Bin64
 xcopy /y %SOURCE_DIR%\Bin64\CrysisVR.exe %CRYSIS_INSTALL_DIR%\Bin64
 xcopy /y %SOURCE_DIR%\Code\ThirdParty\bhaptics\bin\win64\haptic_library.dll %CRYSIS_INSTALL_DIR%\Bin64
+xcopy /y %SOURCE_DIR%\Code\ThirdParty\ForceTubeVR\bin\ForceTubeVR_API_x64.dll %CRYSIS_INSTALL_DIR%\Bin64
 xcopy /i /s /y %SOURCE_DIR%\Game %VRMOD_INSTALL_DIR%\Game
 xcopy /y %SOURCE_DIR%\README.md %VRMOD_INSTALL_DIR%
 xcopy /y %SOURCE_DIR%\LICENSE.txt %VRMOD_INSTALL_DIR%
--- install32.bat
+++ install32.bat
@@ -11,6 +11,7 @@ xcopy /i /y %SOURCE_DIR%\*.xml %VRMOD_INSTALL_DIR%
 xcopy /y %SOURCE_DIR%\Bin32\VRMod.dll %VRMOD_INSTALL_DIR%\Bin32
 xcopy /y %SOURCE_DIR%\Bin32\CrysisVR.exe %CRYSIS_INSTALL_DIR%\Bin32
 xcopy /y %SOURCE_DIR%\Code\ThirdParty\bhaptics\bin\win32\haptic_library.dll %CRYSIS_INSTALL_DIR%\Bin32
+xcopy /y %SOURCE_DIR%\Code\ThirdParty\ForceTubeVR\bin\ForceTubeVR_API_x32.dll %CRYSIS_INSTALL_DIR%\Bin32
 xcopy /i /s /y %SOURCE_DIR%\Game %VRMOD_INSTALL_DIR%\Game
 xcopy /y %SOURCE_DIR%\README.md %VRMOD_INSTALL_DIR%
 xcopy /y %SOURCE_DIR%\LICENSE.txt %VRMOD_INSTALL_DIR%
